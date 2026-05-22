5f876c0ff3cd75ba917f76d86ad4037dec4f73c4
parsiarestaurant <titanhassan@gmail.com>
2026-05-22 11:42:50 +0200

    Add logo to QR code generator

--- generate_qr.py
+++ generate_qr.py
@@ -5,8 +5,8 @@ import os
 # ----------------------
 # تنظیمات
 # ----------------------
-base_url = "https://parsia-restaurant.onrender.com/frontend/menu.html?table="
-tables = 10
+base_url = "https://parsiarestaurant.github.io/parsia-restaurant/frontend/menu.html?table="
+tables = 16
 output_folder = "qrcodes"
 
 os.makedirs(output_folder, exist_ok=True)
